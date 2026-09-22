6dafb15eb6a10367a8b877b47c88e3b96ce5e0fd
tonitarung099-creator <tonitarung099@gmail.com>
2026-09-22 18:23:33 +0700

    fix: safely open source URLs from details

--- src/DownloadAja.Desktop/DownloadDetailsWindow.xaml.cs
+++ src/DownloadAja.Desktop/DownloadDetailsWindow.xaml.cs
@@ -65,14 +65,27 @@ public partial class DownloadDetailsWindow : Window
 
     private void OpenUrl_Click(object sender, RoutedEventArgs e)
     {
-        if (!Uri.TryCreate(_item.Url, UriKind.Absolute, out var uri))
+        if (!Uri.TryCreate(_item.Url, UriKind.Absolute, out var uri) ||
+            (uri.Scheme != Uri.UriSchemeHttp && uri.Scheme != Uri.UriSchemeHttps))
             return;
 
-        Process.Start(new ProcessStartInfo
+        try
         {
-            FileName = uri.AbsoluteUri,
-            UseShellExecute = true
-        });
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = uri.AbsoluteUri,
+                UseShellExecute = true
+            });
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(
+                this,
+                $"URL tidak dapat dibuka di browser.\n\n{ex.Message}",
+                "Download Aja",
+                MessageBoxButton.OK,
+                MessageBoxImage.Warning);
+        }
     }
 
     private void OpenFolder_Click(object sender, RoutedEventArgs e)
